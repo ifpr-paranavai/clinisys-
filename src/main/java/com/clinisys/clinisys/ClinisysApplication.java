ed3843121ae554b8fc575afc838348ada757c3a7
IgorLuigi <igor.luigi@hotmail.com>
2022-05-03 19:04:38 -0300

    first commit

---
+++ src/main/java/com/clinisys/clinisys/ClinisysApplication.java
@@ -0,0 +1,13 @@
+package com.clinisys.clinisys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinisysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinisysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinisys/clinisys/ClinisysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinisys.clinisys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinisysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
