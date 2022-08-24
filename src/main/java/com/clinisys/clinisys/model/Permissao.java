f3285a3edf9a500bbe199fcf743660d55833ff0b
IgorLuigi <igor.luigi@hotmail.com>
2022-08-24 10:22:48 -0300

    Cadastro de Permissões

---
+++ src/main/java/com/clinisys/clinisys/model/Permissao.java
@@ -0,0 +1,53 @@
+package com.clinisys.clinisys.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "permissao")
+public class Permissao implements Serializable {
+
+	public Permissao() {
+		super();
+	}
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne
+	private Funcionario funcionario;
+	@ManyToOne
+	private Funcao funcao;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	public Funcao getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(Funcao funcao) {
+		this.funcao = funcao;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
---
+++ src/main/java/com/clinisys/clinisys/repository/PermissaoRepositorio.java
@@ -0,0 +1,9 @@
+package com.clinisys.clinisys.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.clinisys.clinisys.model.Permissao;
+
+public interface PermissaoRepositorio extends JpaRepository<Permissao, Long>{
+
+}
